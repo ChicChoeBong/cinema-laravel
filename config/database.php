f4ba97ec3f016e697848e18c84ff93c9d5dd53ca
anhquoc2010 <muicamau007@gmail.com>
2023-12-17 01:51:09 +0700

    composer update

--- config/database.php
+++ config/database.php
@@ -35,6 +35,12 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_URI'),
+            'database' => 'YOUR_DATABASE_NAME',
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
